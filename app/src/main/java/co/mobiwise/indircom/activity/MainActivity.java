5f923333666c8620fa43c04d94b644b71ed9485c
Murat Can BUR <muratcanbur@gmail.com>
2015-03-22 13:04:21 +0200

    voting-page-ui merge edildikten sonra gerekli düzenlemeler yapıldı

--- app/src/main/java/co/mobiwise/indircom/activity/MainActivity.java
+++ app/src/main/java/co/mobiwise/indircom/activity/MainActivity.java
@@ -6,11 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import co.mobiwise.indircom.R;
+import co.mobiwise.indircom.fragment.MainVotingPageFragment;
 import co.mobiwise.indircom.listener.ConnectivityChangeListener;
 import co.mobiwise.indircom.listener.PagerCurrentItemListener;
 
 
-public class MainActivity extends ActionBarActivity implements ConnectivityChangeListener {
+public class MainActivity extends ActionBarActivity implements ConnectivityChangeListener, PagerCurrentItemListener {
 
     private static final String TAG = "MainActivity";
 
@@ -24,30 +25,6 @@ public class MainActivity extends ActionBarActivity implements ConnectivityChang
                     .commit();
         }
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     public void onConnectionLost() {
