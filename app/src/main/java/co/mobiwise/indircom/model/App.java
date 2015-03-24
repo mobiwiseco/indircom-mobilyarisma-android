2cef017822b972cca75a41ef62ed5e538e8c1aae
mrtsmsk1 <mrtsmsk1@gmail.com>
2015-03-24 17:52:48 +0200

    edited not camelcase shits.

--- app/src/main/java/co/mobiwise/indircom/model/App.java
+++ app/src/main/java/co/mobiwise/indircom/model/App.java
@@ -7,78 +7,78 @@ import java.io.Serializable;
  */
 public class App implements Serializable{
 
-    private int app_id;
-    private String app_name;
-    private String app_description;
-    private String app_image_url;
-    private String app_download_url;
-    private int user_vote;
+    private int appId;
+    private String appName;
+    private String appDescription;
+    private String appImageUrl;
+    private String appDownloadUrl;
+    private int userVote;
 
     public App() {
     }
 
-    public App(int app_id, String app_name, String app_description, String app_image_url, String app_download_url) {
-        this.app_id = app_id;
-        this.app_name = app_name;
-        this.app_description = app_description;
-        this.app_image_url = app_image_url;
-        this.app_download_url = app_download_url;
+    public App(int appId, String appName, String appDescription, String appImageUrl, String appDownloadUrl) {
+        this.appId = appId;
+        this.appName = appName;
+        this.appDescription = appDescription;
+        this.appImageUrl = appImageUrl;
+        this.appDownloadUrl = appDownloadUrl;
     }
 
-    public int getApp_id() {
-        return app_id;
+    public int getAppId() {
+        return appId;
     }
 
-    public void setApp_id(int app_id) {
-        this.app_id = app_id;
+    public void setAppId(int appId) {
+        this.appId = appId;
     }
 
-    public String getApp_name() {
-        return app_name;
+    public String getAppName() {
+        return appName;
     }
 
-    public void setApp_name(String app_name) {
-        this.app_name = app_name;
+    public void setAppName(String appName) {
+        this.appName = appName;
     }
 
-    public String getApp_description() {
-        return app_description;
+    public String getAppDescription() {
+        return appDescription;
     }
 
-    public void setApp_description(String app_description) {
-        this.app_description = app_description;
+    public void setAppDescription(String appDescription) {
+        this.appDescription = appDescription;
     }
 
-    public String getApp_image_url() {
-        return app_image_url;
+    public String getAppImageUrl() {
+        return appImageUrl;
     }
 
-    public void setApp_image_url(String app_image_url) {
-        this.app_image_url = app_image_url;
+    public void setAppImageUrl(String appImageUrl) {
+        this.appImageUrl = appImageUrl;
     }
 
-    public String getApp_download_url() {
-        return app_download_url;
+    public String getAppDownloadUrl() {
+        return appDownloadUrl;
     }
 
-    public void setApp_download_url(String app_download_url) {
-        this.app_download_url = app_download_url;
+    public void setAppDownloadUrl(String appDownloadUrl) {
+        this.appDownloadUrl = appDownloadUrl;
     }
 
     public int getUserVote() {
-        return user_vote;
+        return userVote;
     }
 
-    public void setUserVote(int user_vote) {
-        this.user_vote = user_vote;
+    public void setUserVote(int userVote) {
+        this.userVote = userVote;
     }
 
     @Override
     public String toString() {
-        return  "App id :" + app_id + "\n"+
-                "App Name  :" + app_name + "\n"+
-                "App Description :" + app_description + "\n"+
-                "App Image URL :" + app_image_url + "\n"+
-                "App Download URL:" + app_download_url + "\n";
+        return  "App id :" + appId + "\n"+
+                "App Name  :" + appName + "\n"+
+                "App Description :" + appDescription + "\n"+
+                "App Image URL :" + appImageUrl + "\n"+
+                "App Download URL:" + appDownloadUrl + "\n";
     }
 }
